cc50fbbf0ee11d260d679e78b5867b449b0d1030
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-07-05 11:52:33 -0300

    implementa a conexão com um banco de dados hospedado no MongoDB Atlas e implementa uma primeira versão de routes+controllers de signup e login para realizar o deploy do Back e já poder testá-lo

---
+++ src/controllers/authController.js
@@ -0,0 +1,96 @@
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import { db } from '../databases/mongodb.js';
+import joi from 'joi';
+
+export async function signUpUser(req, res) {
+  
+    const user = req.body;
+
+    const userSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        confirmPassword: joi.string().required()
+    });
+
+    const { error } = userSchema.validate(user);
+    if (error) {
+        res.status(401).send('Campos inválidos');
+        return;
+    }
+
+    if (user.password !== user.confirmPassword) {
+        res.status(401).send('Senhas não conferem');
+        return;
+    }
+
+    try {
+
+        const userDB = await db.collection('users').findOne({ email: user.email });
+        if (userDB) {
+            res.status(409).send('E-mail já cadastrado');
+            return;
+        }
+
+        const encryptedPassword = bcrypt.hashSync(user.password, 10);
+        delete user.confirmPassword;
+
+        await db.collection('users').insertOne({ ...user, password: encryptedPassword });
+        res.status(201).send('Usuário criado com sucesso');
+
+    } catch (error) {
+        res.sendStatus(500);
+    }
+
+    
+}
+
+export async function loginUser(req, res) {
+    
+    const user = req.body;
+
+    const userSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    });
+
+    const { error } = userSchema.validate(user);
+
+    if (error) {
+        res.status(401).send('Campos inválidos');
+        return;
+    }
+
+    try {
+
+        const userDB = await db.collection('users').findOne({ email: user.email });
+
+        if (userDB && bcrypt.compareSync(user.password, userDB.password)) {
+            const token = uuid();
+
+            let userFirstName = userDB.name;
+            userFirstName = userFirstName.trim();
+            let spacePosition = userFirstName.indexOf(" ");
+            if (spacePosition > -1) {
+                userFirstName = userFirstName.slice(0 , spacePosition);
+            }
+            
+            await db.collection('sessions').insertOne({
+            token,
+            userId: userDB._id
+            });
+
+            res.status(201).send({ token: token, name: userFirstName });
+            return;
+
+        } else {
+            res.status(401).send('Senha ou email incorretos!');
+            return;
+        }
+
+    } catch (error) {
+        res.sendStatus(500);
+    }
+
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRouter from './routes/authRouter.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(authRouter);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
