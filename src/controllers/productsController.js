7b2d5a70218a25ff13beaa2f576979c299cd9e76
CaioVitor1 <caiovitor33@hotmail.com>
2022-07-07 13:29:09 -0300

    implementa getShopping

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,4 +1,5 @@
-import { db } from '../databases/mongodb.js';
+
+import { db, objectId } from '../databases/mongodb.js';
 
 
 export async function listProducts(req, res) {
@@ -13,4 +14,48 @@ export async function listProducts(req, res) {
         res.sendStatus(500);
     }
 
+}
+
+export async function listShopping(req, res) {
+    try {
+        const { authorization } = req.headers;
+        console.log(authorization)
+        const token = authorization?.replace('Bearer ', '');
+        console.log(token)
+            if(!token) {
+                return res.sendStatus(401);
+            }
+        console.log("vamos buscar o token nas sessoões")
+    
+        const session = await db.collection('sessions').findOne({ token });
+        if (!session) {
+        return res.sendStatus(401)
+        }    
+        console.log(session)
+        // eu preciso retornar apenas as compras do shoppingcart que pertencerem ao ID do usuário
+        const shopping = await db.collection('shoppingcart').find({ userId: session.userId }).toArray();
+        console.log(shopping)
+        if (!shopping) {
+            return res.send("Não encontramos compras nesse ID");
+        }
+        res.status(201).send(shopping); 
+        return
+        } 
+    catch(erro) {
+        console.log(erro)
+        console.log("deu ruim")
+        res.sendStatus(422)
+        return
+    }
+}
+
+export async function postShopping(req, res) {
+    try{
+        await db.collection('shoppingcart').insertOne(req.body) 
+        res.sendStatus(201)
+    }catch(erro) {
+        console.log("deu ruim")
+        res.sendStatus(422)
+        return
+    }
 }
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,8 +1,10 @@
-import { listProducts } from '../controllers/productsController.js';
+import { listProducts, listShopping, postShopping } from '../controllers/productsController.js';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/products', listProducts);
 
+router.get('/shoppingcart', listShopping)
+router.post('/shoppingcart', postShopping)
 export default router;
